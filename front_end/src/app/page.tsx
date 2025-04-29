a79d7ff892afb988d506ab0168fc41a8251bd4fd
Eduard Stere <eduard.stal@gmail.com>
2025-04-29 16:56:48 +0300

    css

--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -3,29 +3,32 @@ import { useState } from "react";
 import WalletConnect from '../components/WalletConnect';
 import NFTGallery from '../components/NFTGallery';
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
-
   const [account, setAccount] = useState<string | null>(null);
 
   return (
     <>
       <header className="site-header">
-        <Link href="/" className="logo-link" aria-label="Etherlink Home">
-          <Image
-            src="/etherlink_logo.png"
-            alt="Etherlink Logo"
-            width={40}
-            height={40}
-            className="site-logo"
-            priority
-          />
-        </Link>
+        <div className="header-left">
+          <Link href="/" className="logo-link" aria-label="Etherlink Home">
+            <Image
+              src="/etherlink_logo.png"
+              alt="Etherlink Logo"
+              width={40}
+              height={40}
+              className="site-logo"
+              priority
+            />
+          </Link>
+          <span className="site-title">EtherLink Demo</span>
+        </div>
+        <div className="header-right">
+          <WalletConnect onConnect={setAccount} />
+        </div>
       </header>
       <main className="app-container">
-        <h1>Trilitech Demo</h1>
-        <WalletConnect onConnect={setAccount} />
         {account && <NFTGallery account={account} />}
       </main>
     </>
